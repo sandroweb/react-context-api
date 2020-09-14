e68fc6a63cf81b7b66a0482a6d5bc42ee9433ed3
Sandro Rodrigues dos Santos <sandro.santos@dbccompany.com.br>
2020-09-14 10:31:14 -0300

    Updates

---
+++ precache-manifest.3970182d96efca3db86f102e18b338cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aacc77a729ba4c1564c2b7133299e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acc34d033bb0232c6cc8",
+    "url": "/static/css/main.cc7cdf0b.chunk.css"
+  },
+  {
+    "revision": "cb39508bb7d711056278",
+    "url": "/static/js/2.92dc9aff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.92dc9aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc34d033bb0232c6cc8",
+    "url": "/static/js/main.c69da996.chunk.js"
+  },
+  {
+    "revision": "4b28072af455e051bb87",
+    "url": "/static/js/runtime-main.95c63677.js"
+  }
+]);
